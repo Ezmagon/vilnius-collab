356ffe6da5ba56a34097e70cdce72787fa1a6bd1
Matthijs Tadema <M.J.Tadema@gmail.com>
2018-08-15 18:32:00 +0200

    Added check for gromacs

--- setup.sh
+++ setup.sh
@@ -9,6 +9,15 @@
 #All with OmpA membrane domain (1QJP)
 #and Anti-GFP nanobody camelid single chain (3OGO)
 
+# Test for gromacs
+which gmx &> /dev/null
+if [[ $? -ne 0 ]]
+then
+	echo "Please install gromacs first and source the RC script"
+	echo "Download gromacs here: http://manual.gromacs.org/documentation/2018.2/download.html"
+	exit 1
+fi
+
 # Define some global variables
 SETUP_ROOT=$(pwd)
 
