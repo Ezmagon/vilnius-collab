2d42444d60bdb6cff9787708e2f4be58c1405b88
Matthijs Tadema <M.J.Tadema@gmail.com>
2018-08-15 18:24:40 +0200

    Made setup into script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Three different lipid compositions
+#
+#1. 100% DOPC
+#2. 70% DOPC; 30% Cholesterol
+#3. 68.5% DOPC; 28.5% Cholesterol; 3% DGS-NTI(Ni)
+#
+#All with OmpA membrane domain (1QJP)
+#and Anti-GFP nanobody camelid single chain (3OGO)
+
+sudo ./install_martinize.sh
